f237ca66bfc39045205e617e261482f578b085f3
lszafirski <lukasz.szafirski@ac-project.net>
2017-08-21 14:06:17 +0200

    Added tests project

---
+++ BIAI/BIAI.Tests/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using BIAI.Network;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BIAI.Tests
+{
+    public static class Extensions
+    {
+        public static double[] NextArrayOfNormalizedDoubles(this Random random, int count)
+        {
+            var values = new double[count];
+            for (int i = 0; i < count; i++)
+            {
+                values[i] = random.NextNormalizedDouble();
+            }
+            return values;
+        }
+
+        public static void AssertIsNormalized(double value)
+        {
+            Assert.IsTrue(value >= 0 && value <= 1);
+        }
+    }
+}
---
+++ BIAI/BIAI.Tests/NeuronTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BIAI.Network;
+
+namespace BIAI.Tests
+{
+    [TestClass]
+    public class NeuronTests
+    {
+        [TestMethod]
+        public void ActivateNeuron_ResultIsNormalizedValue()
+        {
+            const int repetitions = 1000;
+            const int activators = 5;
+            var random = new Random();
+
+            for (int i = 0; i < repetitions; i++)
+            {
+                var inputLayer = new NeuronLayer(activators);
+                var outputLayer = new NeuronLayer(1, inputLayer);
+
+                inputLayer.InsertValues(random.NextArrayOfNormalizedDoubles(activators));
+                outputLayer.Compute();
+                Extensions.AssertIsNormalized(outputLayer.GetValues()[0]);
+            }
+        }
+    }
+}
